626f52fdf333241fa9c0fdd52673be4527979720
Mithun <tkmithun@gmail.com>
2023-01-30 15:12:18 +0530

    Second commit

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
-
+### pymongo is the library which helps to interact with MongoDB using Python ###
 
 import pymongo
 
+### Creating URI for MongoDB ###
+
 client=pymongo.MongoClient('mongodb://localhost:27017/')
+
+### Setting DB's Name ###
 
 mydatabase=client['Students']
 
+### Setting collection's(studentsinfo) Name ###
+
 info=mydatabase.studentsinfo
+
+### Creating Collection ###
 
 records={
     'firstname':'Jhon',
     'lastname':'Wick',
     'rollnumber':'01'
 }
+
+### Inserting The Data into the database-collection ###
 
 data=info.insert_one(records)
 
